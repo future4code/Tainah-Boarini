12fe241c54fc747375ee3bea2f021cc9b0feb4d5
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2020-10-09 15:53:57 -0300

    exercicio 1 interpretação de codigo

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,8 @@
+//------------------------------------------------------------------------------------------------------------------Exercícios de interpretação de código ---------------------------------------------------------------------------------------
+
+//Analise os trechos de código em cada exercício e escreva o que ele faz, como faz e qual será o valor impresso no console. --------------------------------------------------------------------------------
+
+//1. -----------  A função nomeada pede na caixa de prompt para informar um valor da cotação em dólar. Esse valor será armazenado na variável 'cotacao'. Em seguida terá um saída representada pelo 'return' que guardará
+// o resultado da multiplicação feito entre o parâmetro 'valorEmDolar' e a variavel da função 'cotacao'. Após executar a função, haverá uma nova variável 'meuDinheiro' que chamará a função conversorDeMoeda substituindo o parametro pelo número 100.
+//O console log irá imprimir portanto >> R$ 100*cotacao (que dependerá do valor incluso no prompt)
+
