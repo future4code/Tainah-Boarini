4c23ac35187359f306091b8b95803298232163a6
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2020-11-17 17:35:53 -0300

    arrumei erro no maps do ListTripsPage. Agora falta ver a AP veer a API que parece vazia

--- semana10/labex/src/components/publicArea/trip/ListTripsPage.js
+++ semana10/labex/src/components/publicArea/trip/ListTripsPage.js
@@ -1,40 +1,40 @@
 import React from "react";
 import {useHistory} from "react-router-dom";
 import {useRequestData} from "../../../hooks/useRequestData";
-import {CardTrip} from "";
+import {CardTrip} from "../trip/style";
 
-// export default function ListTripsPage() {    
+export default function ListTripsPage() {    
 
-//     //requisição de viagens usando o Get Trips
-//     const baseUrl = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/tainah-dumont/"
+    //requisição de viagens usando o Get Trips
+    const baseUrl = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/tainah-dumont/"
 
-//     const getTrips = useRequestData(`${baseUrl}trips`, [])
+    const getTrips = useRequestData(`${baseUrl}trips`, undefined)
 
-//     //Mudanças de páginas com Router
-//     const history = useHistory();
+    //Mudanças de páginas com Router
+    const history = useHistory();
 
-//     const goHome = () => {
-//         history.push("/")
-//     };
+    const goHome = () => {
+        history.push("/")
+    };
 
-//     const goApplicationForm = () => {
-//         history.push("/ApplicationForm/aquiVaiUmId")
-//     }
+    const goApplicationForm = () => {
+        history.push("/ApplicationForm/aquiVaiUmId")
+    }
     
-//     return (
-//         <div>
-//             <p>Lista de viagens</p>
-//             {getTrips && getTrips.trips.map(trip => {
-//                 return <CardTrip key={trip.id} 
-//                 tripTitle={trip.name}
-//                 tripDescription={trip.description}
-//                 tripName={trip.planet}
-//                 tripDuration={trip.durationInDays}
-//                 tripDate={trip.date}
-//                 />
-//             })}
-//             <button onClick={goHome}>Home</button>
-//             <button onClick={goApplicationForm}>Candidate-se!</button>
-//         </div>
-//     )
-// }
+    return (
+        <div>
+            <p>Lista de viagens</p>
+            {getTrips && getTrips.trips.map(trip => {
+                return <CardTrip key={trip.id} 
+                tripTitle={trip.name}
+                tripDescription={trip.description}
+                tripName={trip.planet}
+                tripDuration={trip.durationInDays}
+                tripDate={trip.date}
+                />
+            })}
+            <button onClick={goHome}>Home</button>
+            <button onClick={goApplicationForm}>Candidate-se!</button>
+        </div>
+    )
+}
