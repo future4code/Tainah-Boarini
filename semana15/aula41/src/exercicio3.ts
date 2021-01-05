4ddb8fefb10af466a008c948ffed6bc95253b58c
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2021-01-05 17:14:20 -0300

    exercicio3 completo

--- semana15/aula41/src/exercicio3.ts
+++ semana15/aula41/src/exercicio3.ts
@@ -1,11 +1,11 @@
 //a) Copie o código acima para um arquivo .ts. Depois, crie um type para representar um post e utilize-o para fazer a tipagem do array posts.
 
-type typePost = {
+type post = {
     autor: string,
     texto: string
 }
 
-const posts: typePost[] = [
+const posts: post[] = [
     {
       autor: "Alvo Dumbledore",
       texto: "Não vale a pena viver sonhando e se esquecer de viver"
@@ -29,3 +29,16 @@ const posts: typePost[] = [
   ]
 
   console.table(posts)
+
+  //b) Quais são as entradas e saídas dessa função? Copie a função para o mesmo arquivo .ts do array de posts e faça a tipagem necessária. 
+  //As entradas são os parametros "post e autorInformado" e a saída é o post com o autor informado
+
+  function buscarPostsPorAutor(posts: post[], autorInformado: string) : post[] {
+    return posts.filter(
+      (post) => {
+        return post.autor === autorInformado
+      }
+    )
+}
+
+console.log(buscarPostsPorAutor(posts,"Lord Voldemort"))
