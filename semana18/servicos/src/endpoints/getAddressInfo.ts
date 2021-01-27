ca8399584f85e0d088c6cc0c39e97799733df6ae
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2021-01-27 14:29:33 -0300

    Exercício 1 - função para encontrar cep e retornar um objeto

---
+++ semana18/servicos/src/endpoints/getAddressInfo.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express"
+
+//---------------------aqui vai receber o cep digitado 
+
+
+export const getAddressInfo = async (req: Request, res: Response) => {
+
+    let errorCode: number = 400;
+    
+    try {
+
+        const cep = req.params.cep
+
+        if(!cep || isNaN(Number(cep || cep.includes("." || "-")))) {
+            throw new Error ("cep inválido")
+        }
+
+        // ---------------- aqui, se o cep for válido, vai buscar no banco da api do viacep
+
+
+        const resultAddress = await getAddresByCep(cep) // essa função leva pro service addressManager porque vai pegar uma informação externa 
+
+        res.status(200).send(`Sucesso na verificação do cep: ${resultAddress}`)
+
+
+    } catch (error) {
+        res.status(errorCode).send({message: error.message})
+    }
+
+}
---
+++ semana18/servicos/src/types/address.ts
@@ -0,0 +1,8 @@
+export type address = {
+
+    street: string,
+    neighbourhood: string,
+    city: string,
+    state: string
+
+}
