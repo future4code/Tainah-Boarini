3ed527bc9179eda0dacba8490a25a78467604c37
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2021-02-02 14:23:16 -0300

    Separação de rotas

--- semana19/arquitetura_software2/src/index.ts
+++ semana19/arquitetura_software2/src/index.ts
@@ -1,17 +1,14 @@
 import express from 'express'
 import cors from 'cors'
-import { signup, login } from './controller/userController'
-import { createTask,getTaskById } from './controller/taskController'
+import { userRouter } from './controller/routes/userRouter'
+import { taskRouter } from './controller/routes/taskRouter'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/user/signup', signup)
-app.post('/user/login', login)
-
-app.put('/task', createTask)
-app.get('/task/:id', getTaskById)
+app.use('/user', userRouter)
+app.use('/task', taskRouter)
 
 app.listen(3003, () => {
    console.log('Servidor rodando na porta 3003')
