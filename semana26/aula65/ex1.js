75c08bcf4435a441e755d060d6b12593fd5e778b
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2021-03-16 12:36:45 -0300

    Definição de complexidade algoritmica ex1.

--- semana26/aula65/ex1.js
+++ semana26/aula65/ex1.js
@@ -1,13 +1,11 @@
-const findFirstRecurringCharacters = (input: any) => {
+const findFirstRecurringCharacters = (input) => {
     const charHashMap = {};
     for (const char of input) {
         if (charHashMap[char] === true) {
             return char;
         }
         charHashMap[char] = true;
-        console.log(charHashMap)
+        console.log(charHashMap);
     }
     return null;
-  };
-
-  // A complexidade algoritmica da função é de O(n)
+};
