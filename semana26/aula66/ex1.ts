cca6ee426d488e5d0e9856b4a937833c33e790a2
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2021-03-17 18:01:37 -0300

    exercicios 1 e dois de recursividade

---
+++ semana26/aula66/ex1.ts
@@ -0,0 +1,47 @@
+// const integerNumber = (input: number) => {
+
+//     if (input >= 0) {
+
+//         integerNumber(input - 1)
+//         console.log(input)
+//     }
+// }
+
+// integerNumber(5)
+
+//---------------------------------------------------------
+
+
+// const integerNumberInverte = (input: number) => {
+
+//     if (input >= 0) {
+
+//         console.log(input)
+//         integerNumberInverte(input - 1)
+//     }
+// }
+
+// integerNumberInverte(5)
+
+//---------------------------------------------------------
+
+//Escreva uma função recursiva que calcule a soma dos números inteiros de 0 a **n**
+
+// export const calculateSumTo = (n: number, acc: number = 0): number => {
+//     if (n === 0) {
+//         return acc;
+//     }
+//     return calculateSumTo(n - 1, acc + n);
+// };
+
+// console.log(calculateSumTo(3));
+
+
+const fatorial = (n: number) => {
+
+    if (n < 2) {
+        return 1;
+    }
+    return n * fatorial(n - 1); 
+}
+console.log(fatorial(3));
